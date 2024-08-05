6b1912a3074d7e645d4ff05ff714e02789672b92
JGGrande <joaogabriel7303@gmail.com>
2024-08-05 13:37:26 -0300

    Adiciona react-query e axios

--- src/routes/stack.route.tsx
+++ src/routes/stack.route.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import localStorage from "../database";
 import LoginScreen from "../screens/Login";
 import { ScreenParams } from "./stack.params";
@@ -8,16 +9,20 @@ import TabRoutes from "./tab.routes";
 const Stack = createNativeStackNavigator<ScreenParams>();
 
 export default function StackRoutes(){
+  const queryClient = new QueryClient();
+
   const userIsAuth = localStorage.getItem("userIsAuth");
 
   const initialRoute = userIsAuth ? "Initial" : "Login";
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialRoute}  screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Initial" component={TabRoutes} />
-      </Stack.Navigator>
+      <QueryClientProvider client={queryClient}>
+        <Stack.Navigator initialRouteName={initialRoute}  screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Initial" component={TabRoutes} />
+        </Stack.Navigator>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 }
