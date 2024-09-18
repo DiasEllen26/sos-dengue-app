01ce4ce9cb2f646ab14b8bba94b47dae928e73fd
JGGrande <joaogabriel7303@gmail.com>
2024-09-18 13:44:13 -0300

    Refatora tela de login com ui kitten

--- src/routes/stack.route.tsx
+++ src/routes/stack.route.tsx
@@ -3,13 +3,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import EvaProvider from '../contexts/eva.context';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 
 
 import localStorage from "../database";
 import LoginScreen from "../screens/Login";
 import { ScreenParams } from "./stack.params";
 import TabRoutes from "./tab.routes";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 const Stack = createNativeStackNavigator<ScreenParams>();
 
@@ -20,15 +21,24 @@ export default function Routes(){
 
   const initialRoute = userIsAuth ? "Initial" : "Login";
 
-
-
   return (
     <NavigationContainer>
+      <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider  {...eva} theme={eva.light}>
         <QueryClientProvider client={queryClient}>
-          <Stack.Navigator initialRouteName={initialRoute} screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Initial" component={TabRoutes} />
+          <Stack.Navigator 
+            initialRouteName={initialRoute} 
+            screenOptions={{ headerShown: false  }}
+          >
+            <Stack.Screen 
+              name="Login" 
+              component={LoginScreen} 
+              options={{ headerShown: false }} 
+            />
+            <Stack.Screen 
+              name="Initial" 
+              component={TabRoutes} 
+            />
           </Stack.Navigator>
         </QueryClientProvider>
       </ApplicationProvider>
